72b7e0b11f49dc29254f0fdc5af2e95e8466e6ec
yogieshwar <konneyogie@gmail.com>
2025-02-25 12:01:53 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import '/node_modules/bootstrap/dist/css/bootstrap.css'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <HashRouter>
        <App />
-    </BrowserRouter>
+     </HashRouter>
+    //    https://Yogieshwar.github.io/Recipe-App
   
     
   
